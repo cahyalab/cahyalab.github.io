8a2f16e16276c6644900fc08bb1d4d47aa7b554d
Cahya <nurafianic17@gmail.com>
2024-11-06 19:27:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// File script.js
+document.addEventListener("DOMContentLoaded", function() {
+    alert("Hello, everyone! Take a sneak peek into the celestial sphere of my world. Watch your step, as this journey may leave you breathless!");
+});
